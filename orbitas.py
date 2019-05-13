28fb31bdc32d4a499cbea76ffc68e835270a277c
gabybosc <gabybosc@gmail.com>
2019-05-13 16:06:47 -0300

    clasifica los distintos SZA

--- orbitas.py
+++ orbitas.py
@@ -22,10 +22,10 @@ tenemos datos desde 10/2014 hasta 02/2018
 """
 
 year = 2015
-path = glob.glob('../../../MAVEN/mag_1s/{}/*/*.sts'.format(year))
-# path =  glob.glob('../../datos/MAG_1s/*.sts')
+# path = glob.glob('../../../MAVEN/mag_1s/{}/*/*.sts'.format(year)) #en desktop
+path =  glob.glob('../../datos/MAG_1s/*.sts') #en laptop
 cantidad_datos = len(path)
-calendario = np.zeros((5 * cantidad_datos, 5)) #la primera columna es el día del año, la segunda es el número de orbita, la tercera dice si cumple el SZA, la cuarta la altitud y la quinta el ZMSO
+calendario = np.zeros((5 * cantidad_datos, 6)) #la primera columna es el día del año, la segunda es el número de orbita, la tercera dice si cumple el SZA, la cuarta la altitud y la quinta el ZMSO
 
 
 # Ajuste de Vignes:
@@ -93,49 +93,71 @@ for i,j in enumerate(path):
         else:
             minimo = np.where( max_acercamiento==np.min(max_acercamiento[np.nonzero(max_acercamiento)]))[0][0] #busca el minimo que no sea cero
 
+            idx = minimo * paso
+            """
+            Clasificación por altitud (debería cumplirse siempre, pero aparentemente no pasa)
+            """
+            altitud = np.linalg.norm(pos[int(idx),:]) - 3390
+
+            if altitud < 1300 and altitud > 300:
+                calendario[i*5+indice,2] = 1
+
             """
             Clasificación por SZA despues poner entre 30 y 60 y luego 60 y 90
             """
-            idx = minimo * paso
 
             SZA = np.arccos(np.clip(np.dot(pos[int(idx)]/np.linalg.norm(pos[int(idx)]), [1,0,0]), -1.0, 1.0))* 180/np.pi
             # print(j, indice, SZA)
             if SZA < 30:
                 calendario[i*5+indice,2] = 1
+            elif SZA > 60:
+                calendario[i*5+indice, 4] = 1
+            else:
+                calendario[i*5+indice, 5] = 1
 
             """
-            Clasificación por altitud
-            """
-            altitud = np.linalg.norm(pos[int(idx),:]) - 3390
-
-            if altitud < 1300 and altitud > 300:
-                calendario[i*5+indice,3] = 1
-
-            """
-            Clasificación por Z_MSO
-            """
+            Clasificación por Z_MSO (ya se cumple porque lo pedí en el if)
             Z_MSO = pos[int(idx),2]
             if Z_MSO > 0:
                 calendario[i*5+indice,4] = 1
+            """
 
 
-np.savetxt('tiempos_{}.txt'.format(year), sorted(calendario, key=lambda x: x[0]), fmt='%10d' ,header= "Día        Órbita        SZA        altitud       Z_MSO", newline="\r\n")
+np.savetxt('tiempos_{}.txt'.format(year), sorted(calendario, key=lambda x: x[0]), fmt='%10d' ,header= "Día        Órbita        Altitud        SZA < 30     SZA entre 30 y 60    SZA > 60", newline="\r\n")
 
-clasific = np.sum(calendario[:,2:], axis =1) #si suma 3, es un día que cumple todo.
+clasific_30 = np.sum(calendario[:,2:3], axis =1) #si suma 2, tiene SZA menor a 30.
+clasific_60 = calendario[:,2] + calendario[:,4]#si suma 2, tiene SZA entre 30 y 60.
+clasific_90 = calendario[:,2] + calendario[:,5] #si suma 2, tiene SZA mayor a 90.
 
-fechas_buenas = np.zeros((len(clasific), 2))
+fechas_SZA30 = np.zeros((len(clasific_30), 2))
+fechas_SZA60 = np.zeros((len(clasific_60), 2))
+fechas_SZA90 = np.zeros((len(clasific_90), 2))
 
-for i in range(len(clasific)):
-    if clasific[i] == 3:
-        fechas_buenas[i,0] = calendario[i,0]
-        fechas_buenas[i,1] = calendario[i,1]
+for i in range(len(calendario)):
+    if clasific_30[i] == 2:
+        fechas_SZA30[i,0] = calendario[i,0]
+        fechas_SZA30[i,1] = calendario[i,1]
+    else:  #está adentro de un else para que sólo chequee el if cuando no es menor a 30
+        if clasific_60[i] == 2:
+            fechas_SZA60[i,0] = calendario[i,0]
+            fechas_SZA60[i,1] = calendario[i,1]
+        else:
+            if clasific_90[i] == 2:
+                fechas_SZA90[i,0] = calendario[i,0]
+                fechas_SZA90[i,1] = calendario[i,1]
 
 #para que me de un array ordenado y sin ceros:
-fechas_buenas_trim = fechas_buenas[~np.all(fechas_buenas == 0, axis=1)]
+fechas_SZA30_trim = fechas_SZA30[~np.all(fechas_SZA30 == 0, axis=1)]
+fechas_SZA60_trim = fechas_SZA60[~np.all(fechas_SZA60 == 0, axis=1)]
+fechas_SZA90_trim = fechas_SZA90[~np.all(fechas_SZA90 == 0, axis=1)]
 
-np.savetxt('fechas_buenas_{}.txt'.format(year), sorted(fechas_buenas_trim, key=lambda x: x[0]), fmt='%10d' ,header= "Las fechas de {} en las cuales se cumple todo".format(year), newline="\r\n")
+np.savetxt('SZA30_{}.txt'.format(year), sorted(fechas_SZA30_trim, key=lambda x: x[0]), fmt='%10d' ,header= "Las fechas de {} en las cuales tenemos SZA < 30º".format(year), newline="\r\n")
 
-doy = np.loadtxt('fechas_buenas_{}.txt'.format(year), skiprows=1)[:,0]
+np.savetxt('SZA60_{}.txt'.format(year), sorted(fechas_SZA60_trim, key=lambda x: x[0]), fmt='%10d' ,header= "Las fechas de {} en las cuales tenemos SZA > 30º y < 60º".format(year), newline="\r\n")
+
+np.savetxt('SZA90_{}.txt'.format(year), sorted(fechas_SZA90_trim, key=lambda x: x[0]), fmt='%10d' ,header= "Las fechas de {} en las cuales tenemos SZA > 60º".format(year), newline="\r\n")
+
+doy = np.loadtxt('SZA30_{}.txt'.format(year), skiprows=1)[:,0]
 month = np.zeros(len(doy))
 for d in range(len(doy)):
     date_orbit = dt.datetime(year, 1, 1) + dt.timedelta(doy[d] - 1) #para convertir el doy en date
@@ -145,4 +167,4 @@ plt.hist(month, 12, range=(1,13))
 plt.xlim(left = 1, right=13)
 plt.xlabel('Mes')
 plt.ylabel('Cantidad de órbitas')
-plt.title('Cantidad mensual de órbitas que cumplen las tres condiciones en {}'.format(year))
+plt.title('Cantidad mensual de órbitas con SZA < 30º en {}'.format(year))
