20b82d68cae7f526449167aabe2b9882704f2af2
gabybosc <gabybosc@gmail.com>
2023-12-28 13:18:27 -0300

    histograma low/high wave season

--- bs_mpb/ordenar_datos/leer_txt.py
+++ bs_mpb/ordenar_datos/leer_txt.py
@@ -1,4 +1,5 @@
 import numpy as np
+from generar_npys import generar_npys_limites
 import pandas as pd
 
 """
@@ -22,29 +23,54 @@ allgroups = np.concatenate((g1, g2, g3, g4))
 # for c in range(len(allgroups.T)):
 #     np.save(path + f"{allgroups[0, c]}", allgroups[1:, c])
 
+generar_npys_limites("../../outputs/allgroups/")
+rbs_1 = np.load("../../outputs/grupo1/Rsd_bs.npy")
+rbs_2 = np.load("../../outputs/grupo2/Rsd_bs.npy")
+rbs_3 = np.load("../../outputs/grupo3/Rsd_bs.npy")
+rbs_4 = np.load("../../outputs/grupo4/Rsd_bs.npy")
+rmpb_1 = np.load("../../outputs/grupo1/Rsd_mpb.npy")
+rmpb_2 = np.load("../../outputs/grupo2/Rsd_mpb.npy")
+rmpb_3 = np.load("../../outputs/grupo3/Rsd_mpb.npy")
+rmpb_4 = np.load("../../outputs/grupo4/Rsd_mpb.npy")
+
+rbs = np.hstack((rbs_1, rbs_2, rbs_3, rbs_4))
+rmpb = np.hstack((rmpb_1, rmpb_2, rmpb_3, rmpb_4))
+
+
+rbslow = np.hstack((rbs_3, rbs_4))
+rmpblow = np.hstack((rmpb_3, rmpb_4))
+
+rbsh = np.hstack((rbs_1, rbs_2))
+rmpbh = np.hstack((rmpb_1, rmpb_2))
+
+
+np.save("../../outputs/allgroups/Rsd_bs_high.npy", rbsh)
+np.save("../../outputs/allgroups/Rsd_mpb_high.npy", rmpbh)
 
 """
 Ahora jacob
 """
 
-jacob = pd.read_csv(f"../../outputs/allgroups/Jacob_catalogue_grupos.csv")
+jacob = pd.read_csv("../../outputs/allgroups/Jacob_catalogue_grupos.csv")
 
-# date = [fecha[:10] for fecha in jacob["TIME"]]
-# time = [fecha[11:] for fecha in jacob["TIME"]]
-# x = jacob["XMSO [km]"]
-# y = jacob["YMSO [km]"]
-# z = jacob["ZMSO [km]"]
-# theta = jacob["thetaBN [deg]"]
-# beta = jacob["beta"]
-# np.save(path + "vup.npy", np.array(jacob["|Vupfine| [km/s]"]))
-# np.save(path + "wci.npy", np.array(jacob["Wci [rad/s]"]))
-# np.save(path + "rci.npy", np.array(jacob["rci [km]"]))
-# np.save(path + "pdyn.npy", np.array(jacob["Pdyn [nPa]"]))
-# np.save(path + "cwpe.npy", np.array(jacob["c/Wpe [km]"]))
-# np.save(path + "cwpi.npy", np.array(jacob["c/Wpi [km]"]))
-# np.save(path + "tau_ci.npy", np.array(jacob["tau_ci [s]"]))
-# np.save(path + "cone_angle.npy", np.array(jacob["cone angle [º]"]))
-# np.save(path + "malf_local.npy", np.array(jacob["Malfven_local"]))
-# np.save(path + "malf_global.npy", np.array(jacob["Malfven_global"]))
-# np.save(path + "Mfms.npy", np.array(jacob["Mfms"]))
-# np.save(path + "Ls.npy", np.array(jacob["Ls [deg]"]))
+date = [fecha[:10] for fecha in jacob["TIME"]]
+time = [fecha[11:] for fecha in jacob["TIME"]]
+x = jacob["XMSO [km]"]
+y = jacob["YMSO [km]"]
+z = jacob["ZMSO [km]"]
+theta = jacob["thetaBN [deg]"]
+beta = jacob["beta"]
+np.save(path + "date.npy", date)
+np.save(path + "time.npy", time)
+np.save(path + "vup.npy", np.array(jacob["|Vupfine| [km/s]"]))
+np.save(path + "wci.npy", np.array(jacob["Wci [rad/s]"]))
+np.save(path + "rci.npy", np.array(jacob["rci [km]"]))
+np.save(path + "pdyn.npy", np.array(jacob["Pdyn [nPa]"]))
+np.save(path + "cwpe.npy", np.array(jacob["c/Wpe [km]"]))
+np.save(path + "cwpi.npy", np.array(jacob["c/Wpi [km]"]))
+np.save(path + "tau_ci.npy", np.array(jacob["tau_ci [s]"]))
+np.save(path + "cone_angle.npy", np.array(jacob["cone angle [º]"]))
+np.save(path + "malf_local.npy", np.array(jacob["Malfven_local"]))
+np.save(path + "malf_global.npy", np.array(jacob["Malfven_global"]))
+np.save(path + "Mfms.npy", np.array(jacob["Mfms"]))
+np.save(path + "Ls.npy", np.array(jacob["Ls [deg]"]))
--- bs_mpb/plots/correlaciones.py
+++ bs_mpb/plots/correlaciones.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from loader import importar_params, importar_posiciones
+from scipy.stats import norm
+
 
 path = "../../../../datos/bs_mpb/"
 """cone angle (última columna de los catalogos de Jacob actualizados), beta_protones, Mfms, Pdyn, Ls y bueno el Z"""
@@ -46,3 +48,39 @@ plt.xlabel(r"$R_{SD}$ MPB")
 cbar = plt.colorbar()
 cbar.ax.set_title(r"$L_s$")
 plt.show()
+
+
+Rsd_low = np.vstack(
+        (
+            np.load("../../outputs/allgroups/Rsd_bs_low.npy").astype(float),
+            np.load("../../outputs/allgroups/Rsd_mpb_low.npy").astype(float),
+        )
+    )
+Rsd_h = np.vstack(
+        (
+            np.load("../../outputs/allgroups/Rsd_bs_high.npy").astype(float),
+            np.load("../../outputs/allgroups/Rsd_mpb_high.npy").astype(float),
+        )
+    )
+
+
+fig = plt.figure()
+fig.subplots_adjust(
+    top=0.95, bottom=0.1, left=0.05, right=0.95, hspace=0.1, wspace=0.15
+)
+
+ax1 = plt.subplot2grid((2, 1), (0, 0))
+ax2 = plt.subplot2grid((2, 1), (1, 0), sharex=ax1)
+
+ax1.hist(Rsd_h[1, :], 20, density=1, alpha=0.5, label="higi")
+ax1.hist(Rsd_low[1, :], 20, density=1, alpha=0.5, label="low")
+(muB, sigmaB) = norm.fit(Rsd_h[1, :])
+ax1.legend()
+ax1.set_xlabel("Standoff Distance (RM) MPB")
+ax1.set_ylabel("Cantidad de eventos")
+ax2.hist(Rsd_h[0, :], 20, density=1, alpha=0.5, label="high")
+ax2.hist(Rsd_low[0, :], 20, density=1, alpha=0.5, label="low")
+ax2.set_xlabel("Standoff Distance (RM) BS")
+ax2.set_ylabel("Cantidad de eventos")
+plt.show()
+
